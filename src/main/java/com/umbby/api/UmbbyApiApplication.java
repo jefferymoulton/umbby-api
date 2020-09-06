6bbdfcd9f32852179272ae7f0dcd46fd12cc5811
jefferymoulton <jeff@jefferymoulton.com>
2020-09-06 00:24:56 -0700

    Initial commit

---
+++ src/main/java/com/umbby/api/UmbbyApiApplication.java
@@ -0,0 +1,13 @@
+package com.umbby.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbbyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmbbyApiApplication.class, args);
+    }
+
+}
